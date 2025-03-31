404f1c5fbaec99af8d0b1e3c5bc7bb74c0c77ed9
Sususu <iamsummer614@gmail.com>
2025-03-31 14:57:09 +0900

    post create/read 기능 업데이트

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def signup(request):
@@ -34,3 +35,9 @@ def login(request):
     }
     
     return render(request, 'login.html', context)
+
+
+# logout 기능 함수 구현
+def logout(request):
+    auth_logout(request)
+    return redirect('posts:index')  
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -14,11 +15,17 @@ def index(request):
     return render(request, 'index.html', context)
 
 # Create 함수
+@login_required
+# 로그인한 접속자만 create 기능을 쓸 수 있음
 def create(request):
     if request.method == 'POST': 
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            #작성자에 대한 정보를 넣어줘야함
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            
             return redirect('posts:index')
     else:
         # Get 요청 처리
