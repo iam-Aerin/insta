66c77f628dc93123b10d93f6eb54566c94118510
Sususu <iamsummer614@gmail.com>
2025-04-01 10:44:54 +0900

    Comment create 기능 구현

--- posts/forms.py
+++ posts/forms.py
@@ -1,11 +1,34 @@
 # 새로 만듦 create form을 위해서
 
 from django.forms import ModelForm
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(ModelForm):
     class Meta():
         model = Post
         # fields = '__all__'
         fields = ('content', 'image',)
+        
+
+ # 댓글 작성
+class CommentForm(ModelForm):
+     class Meta():
+         model = Comment
+         fields = ('content',)
+         # fields = ('content', 'image',)
+         
+         
+#  # 댓글 수정
+#  class CommentUpdateForm(ModelForm):
+#      class Meta():
+#          model = Comment
+#          fields = ('content',)
+#          # fields = ('content', 'image',)
+         
+         
+#  # 댓글 삭제
+#  class CommentDeleteForm(ModelForm):
+#      class Meta():
+#          model = Comment
+#          fields = ('content',)
         
