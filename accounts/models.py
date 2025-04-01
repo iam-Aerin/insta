e61111661e396f3830b43cc34b57cce648b6a374
Sususu <iamsummer614@gmail.com>
2025-04-01 11:40:39 +0900

    좋아요 기능 관련 모델링

--- accounts/models.py
+++ accounts/models.py
@@ -9,3 +9,15 @@ class User(AbstractUser):
         upload_to='profile'    
     )
     
+    # User와 Post는 1:N관계로 Foreign Key로 연결되어있다. 
+    # django는 우리 모르게 자동으로 post_set (FK) 을 만들고 있었다.
+    
+    # 이외에도 Comment_set이 저장되는 중이었음.
+    # 그리고 좋아요 기능을 위해 post와 user를 다시 연결 시키려고함.
+    # 그때 post_set이 다시 생기는데, 이때 이름 설정을 (related_name을 post_set이 아닌
+    # 다른 이름으로 설정을 해줘야 충돌이 일어나지 않음
+    
+    # 그래서, `models.py` 에서는 `related_name = 'like_posts' 로 설정하면
+    # N:M 관계의 like_posts(MMF) 가 탄생함. 
+    
+    # => db에 posts_post_like_user라는 테이블이 생김. (user_id와 post_id를 저장함)
--- posts/models.py
+++ posts/models.py
@@ -12,8 +12,14 @@ class Post(models.Model):
         crop=['middle', 'center'],
         upload_to='image/%Y/%m'   
     )
-    
+    # 작성자 저장 필드
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    # 좋아요 기능 
+    # 게시물에 좋아요를 누른 사람들을 저장하는 필드
+    
+    # ManyToManyField - 게시물과 좋아요를 누른 사람을 연결하는 중간 테이블 (M:N의 관계)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts')
     
 # 댓글 기능
 class Comment(models.Model):
